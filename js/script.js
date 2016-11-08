664ab43b39e175d2227b9ab4788a53d791589582
Manish Bisht <manish.bisht490@gmail.com>
2016-11-08 15:20:10 +0530

    Loading resume link dynamically

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
  * Created by Manish Bisht on 7/5/2016.
  */
 $(document).ready(function () {
+    var resume = "https://drive.google.com/file/d/0BzRzoZj__GxWd1FZR05WWG5qTTA";
+    document.getElementById('resume-link').href = resume;
     var currentTime = new Date()
     $("#copyright-year").text(currentTime.getFullYear());
 });
