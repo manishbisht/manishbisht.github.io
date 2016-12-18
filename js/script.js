d5e1b21b4a9d87f140047af1f0d34acd83bc8030
Manish Bisht <manish.bisht490@gmail.com>
2016-12-18 07:25:44 +0530

    Resume link updated

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
  * Created by Manish Bisht on 7/5/2016.
  */
 $(document).ready(function () {
-    var resume = "https://drive.google.com/file/d/0BzRzoZj__GxWeUUtQ1dWMmVJNEE";
+    var resume = "http://goo.gl/Rro9Sk";
     document.getElementById('resume-link').href = resume;
     var currentTime = new Date()
     $("#copyright-year").text(currentTime.getFullYear());
