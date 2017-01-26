96c0c669416e294b4167faee5c25a12517a975dd
Manish Bisht <manish.bisht490@gmail.com>
2017-01-26 09:53:48 +0530

    Loading menu using javscript

--- js/script.js
+++ js/script.js
@@ -2,8 +2,25 @@
  * Created by Manish Bisht on 7/5/2016.
  */
 $(document).ready(function () {
+    loadMenu();
     var resume = "http://goo.gl/Rro9Sk";
     document.getElementById('resume-link').href = resume;
     var currentTime = new Date()
     $("#copyright-year").text(currentTime.getFullYear());
 });
+
+function loadMenu() {
+    var menuhtml = '<a class="mdl-navigation__link" href="index">Overview</a>';
+    menuhtml += '<a class="mdl-navigation__link" href="skills">Skills</a>';
+    menuhtml += '<a class="mdl-navigation__link" href="work-experience">Work Experience</a>';
+    menuhtml += '<a class="mdl-navigation__link" href="competitive-programming">Competitive Programming</a>';
+    menuhtml += '<a class="mdl-navigation__link" href="open-source">Open Source</a>';
+    menuhtml += '<a class="mdl-navigation__link" href="education">Education</a>';
+    menuhtml += '<a class="mdl-navigation__link" href="projects">Projects</a>';
+    menuhtml += '<a class="mdl-navigation__link" id="resume-link" target="_blank">Resume</a>';
+    menuhtml += '<a class="mdl-navigation__link" href="https://github.com/manishbisht" target="_blank">Code on Github</a>';
+    menuhtml += '<a class="mdl-navigation__link" href="https://medium.com/@manishbisht" target="_blank">Blog on Medium</a>';
+    menuhtml += '<a class="mdl-navigation__link" href="https://www.quora.com/profile/Manish-Bisht-39" target="_blank">Questions on Quora</a>';
+    menuhtml += '<a class="mdl-navigation__link" href="contact">Contact</a>';
+    $('#main-menu').html(menuhtml);
+}
