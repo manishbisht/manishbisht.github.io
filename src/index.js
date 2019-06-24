2de7b9b302cde59bb2727e5fe242a38069b5b3e8
Manish Bisht <manish.bisht490@gmail.com>
2019-06-24 22:40:21 +0530

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/style.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
